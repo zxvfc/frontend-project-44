5a7d306b8ff137955b4c0a3dbabe1a5d38e35e6f
zxvfc <velya.96@gmail.com>
2023-05-30 23:18:52 +0200

    fixed issues with prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,14 +4,14 @@ import runGame from '../index.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  if (number <= 3) {
-    return number > 1;
+  if (number === 2 || number === 3) {
+    return true;
   }
-  if (number % 2 === 0 || number % 3 === 0) {
+  if (number <= 1 || number % 2 === 0 || number % 3 === 0) {
     return false;
   }
 
-  for (let i = 5; i <= Math.sqrt(); i += 6) {
+  for (let i = 5; i <= Math.sqrt(number); i += 6) {
     if (number % i === 0 || number % (i + 2) === 0) {
       return false;
     }
